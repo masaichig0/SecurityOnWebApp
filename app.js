c00ea09a202a14e7ce1e77a38e00ca99608aa14b
Masa Ichigo <masahiro_ichigo@hotmail.com>
2023-04-05 08:38:26 -0700

    level 3/hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 const app = express();
@@ -20,8 +20,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-//Create those before Create mongoose model.
-userSchema.plugin(encrypt, { secret: process.env.SECRET_KEY, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -50,7 +48,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username, 
-        password: req.body.password,
+        password: md5(req.body.password),
     });
 
     newUser.save().then(() => {
@@ -63,14 +61,14 @@ app.post("/register", function(req, res) {
 
 app.post("/login", async function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     try {
         const foundUser = await User.findOne({ email: username });
         if (foundUser && foundUser.password === password) {
             res.render("secrets");
         } else {
-            res.render("login", { error: "Invalid password.", email: username, password: password });
+            res.render("login", { error: "Invalid password." });
         }
     } catch (err) {
         console.log(err);
